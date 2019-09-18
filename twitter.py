dada4a52174c3302cd98c777bddfc5ab4e402fe3
cottongin <cottongin@users.noreply.github.com>
2019-09-18 02:21:15 +0000

    refactor extraneous code, add search and url parsing

--- twitter.py
+++ twitter.py
@@ -4,6 +4,7 @@ from sopel.config.types import StaticSection, ValidatedAttribute
 
 import pendulum
 import tweepy
+import re
 
 class APIKey(StaticSection):
     consumer_key = ValidatedAttribute('consumer_key', str)
@@ -11,17 +12,20 @@ class APIKey(StaticSection):
 
 def setup(bot):
     bot.config.define_section('twitter', APIKey)
+    regex = re.compile('.*(twitter.com\/.*\/status\/)([\w-]+).*')
+    if 'url_callbacks' not in bot.memory:
+        bot.memory['url_callbacks'] = {regex: tweetinfo}
+    else:
+        exclude = bot.memory['url_callbacks']
+        exclude[regex] = tweetinfo
+        bot.memory['url_callbacks'] = exclude
 
 def configure(config):
     config.define_section('twitter', APIKey, validate=False)
     config.twitter.configure_setting('consumer_key', '')
     config.twitter.configure_setting('consumer_secret', '')
 
-@sopel.module.commands('twitter', 't')
-@sopel.module.example('.twitter realDonaldTrump')
-def twitter(bot, trigger):
-    """Fetches most recent tweet from provided user"""
-
+def _twitter_auth(bot):
     # Auth stuff
     consumer_key = bot.config.twitter.consumer_key
     consumer_secret = bot.config.twitter.consumer_secret
@@ -29,6 +33,36 @@ def twitter(bot, trigger):
         return bot.say("I need proper Twitter API keys to work")
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     api = tweepy.API(auth)
+    return api
+
+
+def _parse_status(status):
+    try:
+        tweet_text = "RT " + status.retweeted_status.full_text
+    except AttributeError:  # Not a Retweet
+        tweet_text = status.full_text
+    user = status.author.screen_name
+    created_at = pendulum.parse(str(status.created_at), strict=False)
+    if status.author.verified:
+        user += color("✓", "white", "blue")
+    diff = created_at.diff_for_humans()#.in_words()
+    try:
+        # retweets, likes
+        tag = " | 🔃{} ❤️{}".format(
+            status.retweet_count,
+            status.favorite_count,
+        )
+    except:
+        tag = ""
+    reply_string = f"\x02(@{user})\x02 {tweet_text} | {diff}{tag}"
+    return reply_string
+
+@sopel.module.commands('twitter', 't')
+@sopel.module.example('.twitter realDonaldTrump')
+def twitter(bot, trigger):
+    """Fetches most recent tweet from provided user"""
+
+    api = _twitter_auth(bot)
 
     # check for input
     user_input = trigger.group(2)
@@ -40,16 +74,37 @@ def twitter(bot, trigger):
     data = tweepy.Cursor(api.user_timeline, id=f"{user_input}", tweet_mode="extended").items(1)
     try:
         for status in data:
-            try:
-                tweet_text = status.retweeted_status.full_text
-            except AttributeError:  # Not a Retweet
-                tweet_text = status.full_text
-            user = status.author.screen_name
-            created_at = pendulum.parse(str(status.created_at), strict=False)
-            if status.author.verified:
-                user += " " + color("✓", "white", "blue")
-            diff = created_at.diff_for_humans()#.in_words()
-            reply_string = f"\x02@{user}:\x02 {tweet_text} | {diff}"
-            bot.say(reply_string)
+            bot.say(_parse_status(status))
     except:
         return bot.reply("I couldn't find a twitter user by that handle ({})".format(user_input))
+
+@sopel.module.commands('tsearch', 'ts')
+@sopel.module.example('.tsearch donald trump')
+def tsearch(bot, trigger):
+    """Searches twitter"""
+
+    api = _twitter_auth(bot)
+
+    user_input = trigger.group(2)
+    if not user_input:
+        return bot.reply("Uhhh... what do you want to search for?")
+
+    data = api.search(q=f"{user_input}", count=3, result_type="mixed", tweet_mode="extended")
+    if data:
+        for status in data:
+            bot.say(_parse_status(status))
+    else:
+        return bot.reply("I couldn't find any results for that query")
+
+@sopel.module.rule('.*(twitter.com\/.*\/status\/)([\w-]+).*')
+def tweetinfo(bot, trigger, found_match=None):
+    match = found_match or trigger
+    tweet_id = match.group(2)
+    
+    api = _twitter_auth(bot)
+
+    try:
+        status = api.get_status(id=f"{tweet_id}", tweet_mode="extended")
+        bot.say(_parse_status(status))
+    except:
+        return bot.reply("I couldn't find that tweet (was it deleted?)")
