552ae8f5b921f0e3697826d7980958577af451b2
cottongin <cottongin@users.noreply.github.com>
2020-07-27 19:08:41 -0500

    [figlet] add --font support

--- figlet.py
+++ figlet.py
@@ -1,3 +1,5 @@
+import shlex
+
 from sopel import module
 
 from pyfiglet import Figlet
@@ -16,6 +18,30 @@ def setup(bot):
 @module.rate(user=30, channel=10)
 def figlet(bot, trigger):
     """Sends BIG TEXT"""
-    fig = Figlet(font='standard')
-    for let in fig.renderText(trigger.group(2) or "").rsplit("\n"):
-        bot.say(let)
+    valid_args = ["--font"]
+    text = trigger.group(2)
+    if not text:
+        return bot.reply("I need something to figlet")
+    
+    args = {}
+    tmp = shlex.split(text)
+    for index,word in enumerate(tmp):
+        if word in valid_args:
+            args[word.replace("--", "")] = tmp[index + 1]
+            tmp.pop(index)
+            tmp.pop(index)
+    text = " ".join(tmp)
+
+    if not args:
+        font = 'standard'
+    else:
+        font = args['font']
+    
+    try:
+        fig = Figlet(font=font)
+    except:
+        fig = Figlet(font='standard')
+
+    for let in fig.renderText(text.strip() or "").rsplit("\n"):
+        if let.strip():
+            bot.say(let)
